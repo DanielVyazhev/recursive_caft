256ac7a3010b25f2e76cf5c68f3a67a882bbd6e2
Andrey Goncharov <andrey@faillearnrepeat.net>
2026-02-25 17:55:07 +0000

    Add extra models

---
+++ src/experiments/estimate_single_token_entropy/mmlu/llama_70b.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from core.complexity_estimation.complexity_estimation_runner import (
+    ComplexityEstimationRunner,
+    ComplexityEstimationRunnerConfig,
+    ModelGenerateConfig,
+)
+from core.complexity_estimation.entropy.single_token_entropy_estimator import SingleTokenEntropyEstimator
+from core.datasets.mmlu.mmlu_single_token_response_dataset import MMLUSingleTokenResponseDataset, QADatasetConfig
+from core.datasets.qa_dataset_adapter import QADatasetAdapter
+from core.utils.device import DEVICE_MAP
+
+MODEL_NAME = "meta-llama/Llama-3.3-70B-Instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL_NAME, trust_remote_code=True, device_map=DEVICE_MAP, torch_dtype="auto"
+)
+
+ComplexityEstimationRunner(
+    config=ComplexityEstimationRunnerConfig(
+        out_path=str(
+            Path(__file__).parent.joinpath("../../../../data/out/single_token_entropy/mmlu_llama_70b.parquet")
+        ),
+        answer_field_name="model_answer",
+        answer_correctness_field_name="model_answer_correct",
+        generate_config=ModelGenerateConfig(max_new_tokens=1),
+        save_every=100,
+    ),
+    complexity_estimator=SingleTokenEntropyEstimator(),
+).estimate(
+    QADatasetAdapter(
+        MMLUSingleTokenResponseDataset(
+            tokenizer,
+            QADatasetConfig(path=str(Path(__file__).parent.joinpath("../../../../data/source/mmlu_pro_stem.parquet"))),
+        )
+    ),
+    model,
+)
---
+++ src/experiments/estimate_single_token_entropy/mmlu/mistral_24b.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from core.complexity_estimation.complexity_estimation_runner import (
+    ComplexityEstimationRunner,
+    ComplexityEstimationRunnerConfig,
+    ModelGenerateConfig,
+)
+from core.complexity_estimation.entropy.single_token_entropy_estimator import SingleTokenEntropyEstimator
+from core.datasets.mmlu.mmlu_single_token_response_dataset import MMLUSingleTokenResponseDataset, QADatasetConfig
+from core.datasets.qa_dataset_adapter import QADatasetAdapter
+from core.utils.device import DEVICE_MAP
+
+MODEL_NAME = "mistralai/Mistral-Small-3.2-24B-Instruct-2506"
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL_NAME, trust_remote_code=True, device_map=DEVICE_MAP, torch_dtype="auto"
+)
+
+ComplexityEstimationRunner(
+    config=ComplexityEstimationRunnerConfig(
+        out_path=str(
+            Path(__file__).parent.joinpath("../../../../data/out/single_token_entropy/mmlu_mistral_24b.parquet")
+        ),
+        answer_field_name="model_answer",
+        answer_correctness_field_name="model_answer_correct",
+        generate_config=ModelGenerateConfig(max_new_tokens=1),
+        save_every=100,
+    ),
+    complexity_estimator=SingleTokenEntropyEstimator(),
+).estimate(
+    QADatasetAdapter(
+        MMLUSingleTokenResponseDataset(
+            tokenizer,
+            QADatasetConfig(path=str(Path(__file__).parent.joinpath("../../../../data/source/mmlu_pro_stem.parquet"))),
+        )
+    ),
+    model,
+)
