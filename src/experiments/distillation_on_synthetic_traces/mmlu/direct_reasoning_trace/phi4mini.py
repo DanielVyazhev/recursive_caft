3ed74606ed3fd553b18a2616d3d06e5c65415e57
Andrey Goncharov <andrey@faillearnrepeat.net>
2026-06-10 10:55:18 +0300

    Add results

--- src/experiments/distillation_on_synthetic_traces/mmlu/direct_reasoning_trace/phi4mini.py
+++ src/experiments/distillation_on_synthetic_traces/mmlu/direct_reasoning_trace/phi4mini.py
@@ -49,7 +49,7 @@ trainer = LoRATrainer(
                 tokenizer=tokenizer,
             )
         ),
-        training_args=LoRATrainingArgs(num_train_epochs=20, per_device_train_batch_size=8),
+        training_args=LoRATrainingArgs(num_train_epochs=20, per_device_train_batch_size=4),
         lora_training_args=LoRASpecificTrainingArgs(train_thinking_token_embeddings=True),
         save_schedule=[1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20],
     ),
