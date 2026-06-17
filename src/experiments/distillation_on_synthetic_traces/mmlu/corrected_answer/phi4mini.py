051f872023131a26ff2c93fe8083375304e460cd
Andrey Goncharov <andrey@faillearnrepeat.net>
2026-06-17 12:58:22 +0300

    Add results

--- src/experiments/distillation_on_synthetic_traces/mmlu/corrected_answer/phi4mini.py
+++ src/experiments/distillation_on_synthetic_traces/mmlu/corrected_answer/phi4mini.py
@@ -16,6 +16,7 @@ from core.training.lora_trainer import (
     LoRATrainer,
     LoRATrainerConfig,
     LoRATrainingArgs,
+    phi4_mini_lora_target_modules,
 )
 from core.training.thinking_tokens import setup_thinking_tokens
 
@@ -50,7 +51,9 @@ trainer = LoRATrainer(
             )
         ),
         training_args=LoRATrainingArgs(num_train_epochs=20, per_device_train_batch_size=4),
-        lora_training_args=LoRASpecificTrainingArgs(train_thinking_token_embeddings=True),
+        lora_training_args=LoRASpecificTrainingArgs(
+            train_thinking_token_embeddings=True, target_modules=phi4_mini_lora_target_modules
+        ),
         save_schedule=[1, 2, 3, 5, 7, 10, 15, 20],
     ),
     tokenizer=tokenizer,
