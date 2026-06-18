d7da20d8faae72116650805b2cc2533764d962ba
Andrey Goncharov <andrey@faillearnrepeat.net>
2026-06-18 22:39:23 +0100

    Batch size

--- src/experiments/distillation_on_synthetic_traces/mmlu/direct_reasoning_trace/llama_3b_head_truncated24000.py
+++ src/experiments/distillation_on_synthetic_traces/mmlu/direct_reasoning_trace/llama_3b_head_truncated24000.py
@@ -51,7 +51,7 @@ trainer = LoRATrainer(
                 tokenizer=tokenizer,
             )
         ),
-        training_args=LoRATrainingArgs(num_train_epochs=20, per_device_train_batch_size=1),
+        training_args=LoRATrainingArgs(num_train_epochs=20, per_device_train_batch_size=2),
         lora_training_args=LoRASpecificTrainingArgs(train_thinking_token_embeddings=True),
         save_schedule=[1, 3, 5, 10, 15, 20],
     ),
