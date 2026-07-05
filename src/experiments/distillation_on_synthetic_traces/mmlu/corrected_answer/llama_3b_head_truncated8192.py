5716586690657604a12897085af3681779692eef
Andrey Goncharov <andrey@faillearnrepeat.net>
2026-07-05 12:06:30 +0100

    Add exps

--- src/experiments/distillation_on_synthetic_traces/mmlu/corrected_answer/llama_3b_head_truncated8192.py
+++ src/experiments/distillation_on_synthetic_traces/mmlu/corrected_answer/llama_3b_head_truncated8192.py
@@ -1,83 +1,8 @@
-from pathlib import Path
+from experiments.distillation_on_synthetic_traces.mmlu.shared import run
 
-from transformers import AutoTokenizer
-
-from core.datasets.causal_dataset_adapter import CausalDatasetAdapter
-from core.datasets.mmlu.mmlu_reasoning_response_dataset import MMLUReasoningResponseDataset
-from core.datasets.qa_dataset import QADatasetConfig
-from core.datasets.qa_dataset_adapter import QADatasetAdapter
-from core.evaluation.multi_checkpoint_evaluator import (
-    GenerationConfig,
-    MultiCheckpointEvaluator,
-    MultiCheckpointEvaluatorConfig,
+run(
+    model_name="llama_3b",
+    relative_out_path="./corrected_answer/llama_3b_head_truncated8192",
+    train_dataset="train_corrected_answer_deepseek_v4_pro_and_others_head_truncated8192.parquet",
+    save_schedule=[2, 5, 10, 15, 20],
 )
-from core.training.lora_trainer import (
-    LoRASpecificTrainingArgs,
-    LoRATrainer,
-    LoRATrainerConfig,
-    LoRATrainingArgs,
-)
-from core.training.thinking_tokens import setup_thinking_tokens
-
-MODEL_NAME = Path(__file__).parent.joinpath("../../../../../artifacts/base_models_v0/llama_3b").as_posix()
-
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, trust_remote_code=True)
-if tokenizer.pad_token is None:
-    tokenizer.pad_token = tokenizer.eos_token
-setup_thinking_tokens(tokenizer)
-
-OUT_PATH = (
-    Path(__file__)
-    .parent.joinpath(
-        "../../../../../artifacts/distillation_on_synthetic_traces/mmlu/corrected_answer/llama_3b_head_truncated8192"
-    )
-    .as_posix()
-)
-
-trainer = LoRATrainer(
-    config=LoRATrainerConfig(
-        out_path=OUT_PATH,
-        model_id=MODEL_NAME,
-        train_dataset=CausalDatasetAdapter(
-            dataset=MMLUReasoningResponseDataset(
-                config=QADatasetConfig(
-                    path=Path(__file__)
-                    .parent.joinpath(
-                        "../../../../../data/out/splits/random/mmlu/train_corrected_answer_deepseek_v4_pro_and_others.parquet"
-                    )
-                    .as_posix(),
-                    dataset_id="mmlu_train_corrected_answer_deepseek_v4_pro_and_others",
-                ),
-                tokenizer=tokenizer,
-            )
-        ),
-        training_args=LoRATrainingArgs(num_train_epochs=20, per_device_train_batch_size=8),
-        lora_training_args=LoRASpecificTrainingArgs(train_thinking_token_embeddings=True),
-        save_schedule=[1, 2, 3, 5, 7, 10, 15, 20],
-    ),
-    tokenizer=tokenizer,
-)
-trainer.train()
-trainer.unload()
-
-cot_evaluator = MultiCheckpointEvaluator(
-    config=MultiCheckpointEvaluatorConfig(
-        checkpoints_dir=OUT_PATH,
-        eval_dataset=QADatasetAdapter(
-            dataset=MMLUReasoningResponseDataset(
-                config=QADatasetConfig(
-                    path=Path(__file__)
-                    .parent.joinpath("../../../../../data/out/splits/random/mmlu/test.parquet")
-                    .as_posix(),
-                    dataset_id="mmlu_random_test",
-                ),
-                tokenizer=tokenizer,
-            ),
-            add_thinking_start_token=True,
-        ),
-        generation=GenerationConfig(max_new_tokens=8500, max_thinking_tokens=8192, max_batch_size=256),
-        summary_filename="summary_reasoning_evals.json",
-    ),
-    tokenizer=tokenizer,
-)
-cot_evaluator.evaluate_all()
---
+++ src/experiments/distillation_on_synthetic_traces/mmlu/corrected_answer/phi4mini_middle_truncated8192_cap2048.py
@@ -0,0 +1,10 @@
+from experiments.distillation_on_synthetic_traces.mmlu.shared import run
+
+run(
+    model_name="phi4_mini",
+    relative_out_path="./corrected_answer/phi4_mini_middle_truncated8192",
+    train_dataset="train_corrected_answer_deepseek_v4_pro_and_others_middle_truncated8192.parquet",
+    save_schedule=[2, 5, 10, 15, 20],
+    max_thinking_tokens=2048,
+    skip_missing_checkpoints=True,
+)
