e4d787284f8f061008b897b382c36c218b28f48c
Andrey Goncharov <andrey@faillearnrepeat.net>
2026-08-01 13:12:55 +0100

    Fix sample once exps

--- src/experiments/distillation_by_metrics/mmlu/student_entropy_once/qwen_3b_head_truncated8192.py
+++ src/experiments/distillation_by_metrics/mmlu/student_entropy_once/qwen_3b_head_truncated8192.py
@@ -1,11 +1,25 @@
+from core.dataset_samplers.base_sampler import BaseDatasetSamplerConfig
 from core.dataset_samplers.student_entropy_sampler import StudentEntropySampler
-from experiments.distillation_by_metrics.mmlu.shared import get_merged_adapter_with_data_mix, run
+from core.datasets.causal_dataset_adapter import CausalDatasetAdapter
+from core.datasets.mmlu.mmlu_reasoning_response_dataset import MMLUReasoningResponseDataset
+from core.datasets.mmlu.mmlu_single_token_response_dataset import QADatasetConfig
+from experiments.distillation_by_metrics.mmlu.shared import run
 
 run(
     model_name="qwen_3b",
     relative_out_path="./student_entropy_once/qwen_3b_head_truncated8192",
     train_dataset="train_corrected_answer_deepseek_v4_pro_and_others_head_truncated8192",
-    train_dataset_adapter=get_merged_adapter_with_data_mix(StudentEntropySampler),
+    train_dataset_adapter=CausalDatasetAdapter(
+        dataset=MMLUReasoningResponseDataset(
+            config=QADatasetConfig(
+                path="should be overridden by SetResamplingPathCallback",
+                dataset_id="train_corrected_answer_deepseek_v4_pro_and_others_head_truncated8192",
+            ),
+            # Will be overridden
+            tokenizer=None,  # type: ignore
+        ),
+        dataset_sampler=StudentEntropySampler(BaseDatasetSamplerConfig(top_k=1024)),
+    ),
     save_schedule=[5, 10, 20, 40, 60, 80, 100],
     resampling_schedule=[0],
 )
