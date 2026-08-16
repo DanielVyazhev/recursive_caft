1c2473ac3e944395b6dd980da0d0ad22f501d0ea
Andrey Goncharov <andrey@faillearnrepeat.net>
2026-08-16 13:40:28 +0300

    Fix exps

--- src/experiments/distillation_by_metrics/mmlu/entropy_gain_proportional/phi4_mini_head_truncated8192_shuffle.py
+++ src/experiments/distillation_by_metrics/mmlu/entropy_gain_proportional/phi4_mini_head_truncated8192_shuffle.py
@@ -1,15 +1,22 @@
 from core.complexity_estimation.entropy.single_token_entropy_with_random_estimator import (
     SingleTokenEntropyWithRandomEstimator,
 )
-from core.dataset_samplers.entropy_gain_proportional_sampler import EntropyGainProportionalSampler
-from experiments.distillation_by_metrics.mmlu.shared import get_merged_adapter_with_data_mix, run
+from core.dataset_samplers.entropy_gain_proportional_sampler import (
+    EntropyGainProportionalSampler,
+)
+from experiments.distillation_by_metrics.mmlu.shared import (
+    get_merged_adapter_with_data_mix,
+    run,
+)
 
 run(
     model_name="phi4_mini",
     relative_out_path="./entropy_gain_proportional/phi4_mini_head_truncated8192_shuffle",
     train_dataset="train_corrected_answer_deepseek_v4_pro_and_others_head_truncated8192",
-    train_dataset_adapter=get_merged_adapter_with_data_mix(EntropyGainProportionalSampler),
-    save_schedule=[10, 20, 50, 80, 100],
+    train_dataset_adapter=get_merged_adapter_with_data_mix(
+        EntropyGainProportionalSampler
+    ),
+    save_schedule=[10, 20, 50, 80, 100, 150, 200],
     complexity_estimator_override=SingleTokenEntropyWithRandomEstimator(),
-    shuffle=True
+    shuffle=True,
 )
