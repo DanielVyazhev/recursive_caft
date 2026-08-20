e7ee58f3c2d1a3d2dcdb49f6da27a36f41f7a614
Andrey Goncharov <andrey@faillearnrepeat.net>
2026-08-20 15:23:25 +0100

    Add exps

--- src/experiments/distillation_by_metrics/mmlu/random/qwen_3b_head_truncated8192.py
+++ src/experiments/distillation_by_metrics/mmlu/random/qwen_3b_head_truncated8192.py
@@ -1,4 +1,6 @@
-from core.complexity_estimation.random_estimator import RandomEstimator
+from core.complexity_estimation.entropy.single_token_entropy_with_random_estimator import (
+    SingleTokenEntropyWithRandomEstimator,
+)
 from core.dataset_samplers.random_sampler import RandomSampler
 from experiments.distillation_by_metrics.mmlu.shared import (
     get_merged_adapter_with_data_mix,
@@ -7,9 +9,10 @@ from experiments.distillation_by_metrics.mmlu.shared import (
 
 run(
     model_name="qwen_3b",
-    relative_out_path="./random/qwen_3b_head_truncated8192_new",
+    relative_out_path="./random/qwen_3b_head_truncated8192",
     train_dataset="train_corrected_answer_deepseek_v4_pro_and_others_head_truncated8192",
     train_dataset_adapter=get_merged_adapter_with_data_mix(RandomSampler),
-    save_schedule=[10, 20, 50, 80, 100, 150, 200],
-    complexity_estimator_override=RandomEstimator(),
+    save_schedule=[150, 200],
+    skip_missing_checkpoints=True,
+    complexity_estimator_override=SingleTokenEntropyWithRandomEstimator(),
 )
