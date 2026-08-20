e13d8ffda57444a67bc7c76ef557460c5ed6c3d8
Andrey Goncharov <andrey@faillearnrepeat.net>
2026-08-20 17:44:34 +0300

    Fix seed

--- src/core/utils/seed.py
+++ src/core/utils/seed.py
@@ -1,9 +1,11 @@
 import random
 import torch
+import numpy
 
 def set_seed():
     seed = 42
     random.seed(seed)
+    numpy.random.seed(seed=seed)
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
